959f8d514f4d342ce2b01d2f6279cc67d3cae570
nupur <nupurrathi2218@gmail.com>
2021-06-22 19:21:15 +0530

    user disconnected condition

--- frontend_teamsClone/src/components/common/GroupButtons.js
+++ frontend_teamsClone/src/components/common/GroupButtons.js
@@ -7,7 +7,7 @@ import PhoneEnabledOutlinedIcon from '@material-ui/icons/PhoneEnabledOutlined';
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
 import { CHAT, VIDEOCALL, AUDIOCALL } from '../../constants';
 import { setClass } from '../../data/actions/classReducerActions';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
   root: { 
@@ -28,12 +28,37 @@ export default function BasicButtonGroup({currentWindow, setWindowState}) {
 
   const dispatch = useDispatch();
 
+  const currSelectedUser = useSelector(state => state.currSelectedReducer);
+  const usersList = useSelector(state => state.usersListReducer);
+
   return (
     <div className={classes.root}>
       <ButtonGroup size="small" aria-label="outlined primary button group">
         <Button onClick={() => {setWindowState(CHAT); dispatch(setClass(false))}}><ChatOutlinedIcon fontSize="small" /></Button>
-        <Button onClick={() => {setWindowState(VIDEOCALL); dispatch(setClass(true))}}><VideocamOutlinedIcon fontSize="small"/></Button>
-        <Button onClick={() => {setWindowState(AUDIOCALL); dispatch(setClass(true))}}><PhoneEnabledOutlinedIcon fontSize="small"/></Button>
+        <Button onClick={() => {
+          if(currSelectedUser.id in usersList === true)
+          {
+            setWindowState(VIDEOCALL); 
+            dispatch(setClass(true));
+          }
+          else
+          {
+            alert("This user has disconnected. Cannot call");
+          }
+        }}>
+          <VideocamOutlinedIcon fontSize="small"/></Button>
+        <Button onClick={() => {
+          if(currSelectedUser.id in usersList === true)
+          {
+            setWindowState(AUDIOCALL); 
+            dispatch(setClass(true));
+          }
+          else
+          {
+            alert("This user has disconnected. Cannot call");
+          }
+        }}>
+          <PhoneEnabledOutlinedIcon fontSize="small"/></Button>
       </ButtonGroup>
     </div>
   );
--- frontend_teamsClone/src/components/video/VideoWindow.js
+++ frontend_teamsClone/src/components/video/VideoWindow.js
@@ -1,25 +1,125 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../../styles/videoWindow.css';
 import CallEndRoundedIcon from '@material-ui/icons/CallEndRounded';
 import { CHAT } from "../../constants";
 import { useDispatch, useSelector } from 'react-redux';
 import { setClass } from '../../data/actions/classReducerActions';
+import Peer from 'simple-peer';
 
 const VideoWindow = ({setWindowState}) => {
 
+    const [stream, setStream] = useState(null);
+    const [pstream, setPStream] = useState(null);
+    const [call, setCall] = useState({});
+    const [callaccepted , setCallaccepted] = useState(false);
+    const [callended , setCallended] = useState(false);
+    const [userMedia, setUserMedia] = useState({ video: true, audio: true });
+
+    const myvideo = useRef();
+    const othervid = useRef();
+    const connref = useRef();
+
+
     const dispatch = useDispatch();
-    const user = useSelector();
+    const {name, id, email, socket, imgUrl} = useSelector(state => state.userReducer);
+
+    useEffect(() => {
+
+        navigator.mediaDevices.getUserMedia(userMedia)
+        .then((currentStream) => {
+            setStream(currentStream);
+            myvideo.current.srcObject = currentStream;
+        });
+
+        socket.current.on("calluser", ({from, signal}) => {
+            setCall({isreceivedcall: true, from, signal});
+        });
+    
+    }, []);
+
+    // const answercall = () => {
+
+    //     setCallaccepted(true);
+    
+    //     const peer = new Peer({
+    //       initiator: false,
+    //       trickle: false,
+    //       stream: stream,
+    //     });
+    
+    //     peer.once("signal", data => {
+    //       socket.current.emit("answercall", {signal: data, to: call.from})
+    //     });
+    
+    //     peer.once("stream", currStream => {
+    //       setPStream(currStream);
+    //       othervid.current.srcObject = currStream;
+    //     });
+    
+    //     peer.signal(call.signal);
+    
+    //     connref.current = peer;
+    
+    //     socket.current.once("callended", () => {
+    //       setPStream(null);
+    //       console.log(connref.current);
+    //     });
+    
+    // };
+    
+    // const callUser = (id) => {
+    //     const peer = new Peer({
+    //       initiator: true,
+    //       trickle: false,
+    //       stream: stream,
+    //     });
+    
+    //     peer.once("signal", data => {
+    //       socket.current.emit("calluser", {usertocall: id, from: me, signalData: data})
+    //     });
+    
+    //     peer.once("stream", (currStream) => {
+    //       setPStream(currStream);
+    //       othervid.current.srcObject = currStream;
+    //     });
+    
+    //     socket.once("callaccepted", signal => {
+    //       setCallaccepted(true);
+    //       peer.signal(signal);
+    //     });
+    
+    //     connref.current = peer;
+    
+    //     socket.current.once("callended", () => {
+    //       setPStream(null);
+    //     });
+      
+    // };
+
+    const leaveCall = () => {
+
+        // setCallended(true);
+        // setPStream(null);
+        // connref.current.removeAllListeners('close');
+        // connref.current.destroy();
+        // console.log(connref.current);
+        // socket.current.emit("callend");
+    }
 
     return (
         <div className="videoWindow">
             <div className="videoLeft">
                 <div className="videoMain">
-                    <div className="userVideos"></div>
-                    <div className="userVideos"></div>
+                    <div className="userVideos">
+                        {stream && (<video playsInline muted ref={myvideo} autoPlay className="video" />)}
+                    </div>
+                    {/* <div className="userVideos">
+                    </div> */}
                 </div>
                 <div className="videoOptions">
-                    <button className="videoOptionsButtons videoOptionsEndcall" onClick={() => {setWindowState(CHAT); dispatch(setClass(false))}}>
-                        <CallEndRoundedIcon fontSize="medium" />
+                    <button className="videoOptionsButtons videoOptionsEndcall" onClick={() => 
+                        { setWindowState(CHAT); leaveCall(); dispatch(setClass(false))}}>
+                        <CallEndRoundedIcon fontSize="default" />
                     </button>
                 </div>
             </div>
